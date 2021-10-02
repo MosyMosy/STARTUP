ff59b15e7bae5f637c743835ec03421a671f7975
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-10-02 14:02:19 +0330

    teacher configs

--- teacher_miniImageNet_na/configs.py
+++ teacher_miniImageNet_na/configs.py
@@ -1,6 +1,11 @@
 # TODO: Set the directory to save the model
 save_dir = './logs_deterministic'
 
-# TODO: Set the directory to the miniImageNet/tieredImageNet dataset
-miniImageNet_path = '/scratch/datasets/miniImageNet_full_resolution/train'
-tiered_ImageNet_path = '/scratch/datasets/tiered_imagenet/tiered_imagenet/original_split/train'
+# TODO: Please set the directory to the target datasets accordingly
+miniImageNet_path = '/scratch/datasets/CD-FSL/miniImageNet_test'
+tiered_ImageNet_path = '/scratch/datasets/tiered_imagenet/tiered_imagenet/original_split/test'
+
+ISIC_path = "../dataset/ISIC2018"
+ChestX_path = "../dataset/ChestX-Ray8"
+CropDisease_path = "../dataset/plant-disease"
+EuroSAT_path = "../dataset/EuroSAT/2750"
