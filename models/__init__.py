8f24178da3b9543dafc372400267cfacde8b9824
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-11-22 10:45:04 +0330

    Resnet10 with GroupNorm

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .resnet import *
 from .dataparallel_wrapper import *
 from .resnet10 import ResNet10
+from .resnet10_GN import ResNet10_GN
 from .resnet12 import Resnet12
--- teacher_miniImageNet_na/run.sh
+++ teacher_miniImageNet_na/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 
+# training a classification model with GroupNorm on miniImageNet
+python train.py --dataset miniImageNet --model ResNet10_GN  --method baseline --bsize 256 --start_epoch 0 --stop_epoch 400 --train_aug 
+
 # training a classification model on miniImageNet
-python train.py --dataset miniImageNet --model ResNet10  --method baseline --bsize 256 --start_epoch 0 --stop_epoch 400 --train_aug 
+# python train.py --dataset miniImageNet --model ResNet10  --method baseline --bsize 256 --start_epoch 0 --stop_epoch 400 --train_aug 
 
 # training a classification model on tieredImageNet
 # python train.py --dataset tiered_ImageNet --model ResNet12  --method baseline --bsize 256 --start_epoch 0 --stop_epoch 90
