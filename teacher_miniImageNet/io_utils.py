332c2c6cda74643ca68838655820dcd35766196b
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-11-22 22:45:46 +0330

    Resnet10 with GroupNorm

--- teacher_miniImageNet/io_utils.py
+++ teacher_miniImageNet/io_utils.py
@@ -17,6 +17,7 @@ def create_resnet18():
 
 model_dict = {
             'ResNet10': models.ResNet10,
+            'ResNet10_GN': models.ResNet10_GN,
             'ResNet12': create_resnet12, 
             'ResNet18': create_resnet18,
 }
