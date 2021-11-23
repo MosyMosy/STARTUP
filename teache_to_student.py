ec75bfa8a60152cebacd9340e8fe8ddd5bc75f1e
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-11-23 17:33:48 +0330

    ResNet10_GN

--- teache_to_student.py
+++ teache_to_student.py
@@ -40,8 +40,8 @@ def checkpoint(model, save_path):
     torch.save(sd, save_path)
     return sd
 
-checkpoint(load_checkpoint(models.ResNet10(
+checkpoint(load_checkpoint(models.ResNet10_GN(
 ), "teacher_miniImageNet/logs_deterministic/checkpoints/miniImageNet/ResNet10_GN_baseline_256_aug/399.tar", torch.device("cpu")), "baseline_teacher_GN/checkpoint_best.pkl")
 
-checkpoint(load_checkpoint(models.ResNet10(
+checkpoint(load_checkpoint(models.ResNet10_GN(
 ), "teacher_miniImageNet_na/logs_deterministic/checkpoints/miniImageNet/ResNet10_GN_baseline_256_aug/399.tar", torch.device("cpu")), "baseline_na_teacher_GN/checkpoint_best.pkl")
